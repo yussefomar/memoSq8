42fd5b8cff59dd1fea7b10687640f1039700ab61
yuscuaro <yuscuaro@gmail.com>
2023-06-12 19:03:05 -0300

    agregando obtencion de entidades por id

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ recursos=[
         "codPersona":1,
         "nombre":"jose"
 
+    },
+    {
+        "codPersona":2,
+        "nombre":"martin"
+
     }
 ]
 
@@ -16,6 +21,11 @@ tareas=[
     {
         "codTarea":1,
         "titulo":"codificar"
+
+    },
+     {
+        "codTarea":2,
+        "titulo":"compilar"
 
     }
 ]
@@ -25,6 +35,11 @@ fechas=[
         "codFecha":1,
         "horasParticulares":12,
         "timeStamp":"15/02/2008"
+    },
+    {
+        "codFecha":2,
+        "horasParticulares":123,
+        "timeStamp":"15/04/2008"
     }
 ]
 
@@ -37,10 +52,22 @@ def read_root():
 def get_recursos():
     return recursos
 
+@app.get("/recursos/{id}")
+def get_recursos(id:int):
+    return list(filter(lambda item:item['codPersona']==id,recursos))
+
 @app.get("/tareas")
 def get_tareas():
     return tareas
 
+@app.get("/tareas/{id}")
+def get_tareas():
+    return list(filter(lambda item:item['codTarea']==id,tareas))
+
 @app.get("/fechas")
 def get_fechas():
     return fechas
+
+@app.get("/fechas/{id}")
+def get_fechas():
+    return list(filter(lambda item:item['codTarea']==id,fechas))
