0cadf69e91e33eda91a5eafdb2c36c36f52e9cfc
nacho <ichacons@fi.uba.ar>
2023-06-26 20:20:56 -0300

    Wtf estoy haciendo, creo que arregla lo de la db

--- config/db.py
+++ config/db.py
@@ -1,3 +1,3 @@
 from sqlalchemy import create_engine ,MetaData
-engine=create_engine("mysql+pymysql://root@localhost:3306/flutter", isolation_level="AUTOCOMMIT")
+engine=create_engine("postgres://root:YAaKfEUJKmPULknsRRuH4Qugjeby0obQ@dpg-cid1q8liuie2ea22e110-a/flutter", isolation_level="AUTOCOMMIT")
 meta_data=MetaData()
