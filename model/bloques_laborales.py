376979af21e64d8a5eea327b55b4e00f33aa60a7
nacho <ichacons@fi.uba.ar>
2023-06-28 09:37:14 -0300

    Arreglo bug en la verificación de los bloques laborales cuando se crean por primera vez para un recurso determinado. Arreglo error en el docker-compose

--- model/bloques_laborales.py
+++ model/bloques_laborales.py
@@ -18,14 +18,13 @@ def bloques_model_horas_del_recurso(data_bloque_laboral: BloqueLaboralSchema):
     try:
         with engine.connect() as conn:
             bloque_nuevo = data_bloque_laboral.dict()
-            horas_cargadas_en_usuario = conn.execute(
+            horas_cargadas_en_usuario = list(conn.execute(
                 text(f"""
                 SELECT SUM(horasDelBloque) 
                 FROM bloques_laborales 
                 WHERE legajo={bloque_nuevo['legajo']} AND fecha='{bloque_nuevo['fecha']}';
-                """))
-            
-            return list(horas_cargadas_en_usuario)[0][0]
+                """)))[0][0]
+            return 0 if not horas_cargadas_en_usuario else horas_cargadas_en_usuario
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail="Ocurrio un error al cargar las horas laborales")
